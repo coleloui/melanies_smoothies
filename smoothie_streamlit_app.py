b846162ca816fbd44e4501b153adc8d13852d34b
Louis Coleman <coleloui18@gmail.com>
2025-02-17 17:20:35 -0800

    sf into ingredients

--- smoothie_streamlit_app.py
+++ smoothie_streamlit_app.py
@@ -41,6 +41,12 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
+        smoothiefroot_response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/watermelon", timeout=300
+        )
+        sf_df = st.dataframe(
+            data=smoothiefroot_response.json(), use_container_width=True
+        )
 
     my_insert_stmt = (
         """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -57,9 +63,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success("Your Smoothie is ordered, " + name_on_order + "!", icon="✅")
-
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon", timeout=300
-)
-st.text(smoothiefroot_response)
-st.text(smoothiefroot_response.json())
