09d0f6f5f9d3ca3688d6e9ef3e865d359bc954f8
Louis Coleman <coleloui18@gmail.com>
2025-02-17 16:50:22 -0800

    adding smoothie froot api

--- smoothie_streamlit_app.py
+++ smoothie_streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -56,3 +57,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success("Your Smoothie is ordered, " + name_on_order + "!", icon="✅")
+
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon", timeout=300
+)
+st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
